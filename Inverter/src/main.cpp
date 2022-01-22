73fcbae7f4959357076e762ec233891dd5e61055
Arnout Luinge <arnout.luinge@gmail.com>
2022-01-22 14:38:34 +0100

    soup

--- Inverter/src/main.cpp
+++ Inverter/src/main.cpp
@@ -1,6 +1,7 @@
 /*
-Inverter code
-Project group 16
+  Inverter code
+  Project group 16
+  Using TTGO-T7 v1.3 esp-32 microcontroller
 */
 
 #include <Arduino.h>
@@ -9,10 +10,18 @@ Project group 16
 //define used hardware pins
 #define LED_pin 19
 #define netVoltMeas_pin 2
+#define invVoltMeas_pin 3
+#define PWMPosOut_pin 4
+#define PWMNegOut_pin 5
 #define test_pin 4
 
+// setting PWM properties
+#define PWMFreq 20000
+#define PWMLedChannel 0
+#define PWMResolution 8
+
 //define variables
-int netVoltMeas, highestValue;
+int netVoltMeas, invVoltMeas, highestValue;
 float netFreq;
 bool signal;
 //unsigned long to not overflow the buffer
@@ -27,6 +36,12 @@ void setup() {
   pinMode(LED_pin, OUTPUT);
   pinMode(netVoltMeas_pin, INPUT);
 
+  // configure LED PWM functionalitites
+  ledcSetup(PWMLedChannel, PWMFreq, PWMResolution);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(PWMPosOut_pin, PWMLedChannel);
+
   Serial.begin(115200);
 
   //initialise some variables
@@ -39,6 +54,9 @@ void setup() {
 int readSensors(){
   //measure and safe the voltage measurements
   netVoltMeas = analogRead(netVoltMeas_pin);
+  invVoltMeas = analogRead(invVoltMeas_pin);
+
+  return 0;
 }
 
 int calculateFrequency(){
@@ -69,6 +87,23 @@ int calculateFrequency(){
     Serial.println("Hz");
     timerMillis = currentMillis;
   }
+
+  return netFreq;
+}
+
+int writePWM(){
+  int PWMDutyCycle;
+
+  PWMDutyCycle = pow(PWMResolution,2)*sin(netFreq*loop);
+
+  //dont write a negative dutycycle
+  if (PWMDutyCycle >= 0){
+    ledcWrite(PWMLedChannel, PWMDutyCycle);
+  } else if (PWMDutyCycle < 0){
+    ledcWrite(PWMLedChannel, -PWMDutyCycle);
+  }
+  
+  return 0;
 }
 
 void loop() {
@@ -82,6 +117,11 @@ void loop() {
   readSensors();
 
   //calculate the frequency every loop
-  calculateFrequency();
+  netFreq = calculateFrequency();
+
+  //
+
+  //write the PWM for the H-bridge
+  writePWM(FreqSetPoint, FreqError, PhaseSetPoint);
 
 }
