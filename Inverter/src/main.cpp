af43ec2dec00e68aa8e8277451d466a4dd57631d
Arnout Luinge <arnout.luinge@gmail.com>
2022-01-22 15:17:55 +0100

    pid implementation

--- Inverter/src/main.cpp
+++ Inverter/src/main.cpp
@@ -18,14 +18,14 @@
 #define PWMNegOut_pin 5
 #define test_pin 4
 
-// setting PWM properties
+//setting PWM properties
 #define PWMFreq 20000
 #define PWMLedChannel 0
 #define PWMResolution 8
 
 //define variables
-int netVoltMeas, invVoltMeas, highestValue;
-float netFreq;
+int netVoltMeas, invVoltMeas, highestValue, loopTimer;
+double netFreq;
 bool signal;
 //unsigned long to not overflow the buffer
 unsigned long currentMillis, zeroPointMillis, timerMillis;
@@ -33,6 +33,12 @@ unsigned long currentMillis, zeroPointMillis, timerMillis;
 CircularBuffer<int, 5> voltMeasBuf; //circular buffer of the voltage measurements
 CircularBuffer<unsigned long, 6> zeroPointMillisBuf; //circular buffer of the zeroPoits in ms
 
+//Define PID Variables we'll be connecting to
+double freqSetPoint, Input, freqOutput;
+
+//Specify the PID links and initial tuning parameters
+double PWMPIDp=2, PWMPIDi=5, PWMPIDd=1;
+PID PWMPID(&netFreq, &freqOutput, &freqSetPoint, PWMPIDp, PWMPIDi, PWMPIDd, DIRECT);
 
 void setup() {  
   //set pin modes
@@ -52,10 +58,13 @@ void setup() {
   zeroPointMillis = 0;
   signal = 0;
 
+  //turn the PWM PID on
+  PWMPID.SetMode(AUTOMATIC);
+
 }
 
 int readSensors(){
-  //measure and safe the voltage measurements
+  //measure and save the voltage measurements
   netVoltMeas = analogRead(netVoltMeas_pin);
   invVoltMeas = analogRead(invVoltMeas_pin);
 
@@ -63,6 +72,7 @@ int readSensors(){
 }
 
 int calculateFrequency(){
+  //calculate the frequency of the net based of the timing between the high points in the measured sinusoidal signal from the net
 
   if (netVoltMeas > highestValue){
       highestValue = netVoltMeas;
@@ -97,7 +107,7 @@ int calculateFrequency(){
 int writePWM(){
   int PWMDutyCycle;
 
-  PWMDutyCycle = pow(PWMResolution,2)*sin(netFreq*loop);
+  PWMDutyCycle = pow(PWMResolution,2)*sin(netFreq*loopTimer);
 
   //dont write a negative dutycycle
   if (PWMDutyCycle >= 0){
@@ -110,7 +120,7 @@ int writePWM(){
 }
 
 void loop() {
-  //delay cause otherwise arduino brakes
+  //delay cause otherwise arduino breakes
   delay(1);
 
   //update currentmillis on every loop
@@ -122,9 +132,10 @@ void loop() {
   //calculate the frequency every loop
   netFreq = calculateFrequency();
 
-  //
+  PWMPID.Compute();
+
 
   //write the PWM for the H-bridge
-  writePWM(FreqSetPoint, FreqError, PhaseSetPoint);
+  //writePWM(FreqSetPoint, FreqError, PhaseSetPoint);
 
 }
