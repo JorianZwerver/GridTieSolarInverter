e33a71ab3f733325137087b050fabe977f79faa2
Arnout Luinge <arnout.luinge@gmail.com>
2022-01-25 12:40:06 +0100

    update

--- Inverter/src/main.cpp
+++ Inverter/src/main.cpp
@@ -17,7 +17,8 @@
 
 //define used hardware pins
 #define LED_pin LED_BUILTIN
-#define netVoltMeas_pin 2
+#define netFreqVoltMeas_pin 2
+#define netPhaseVoltMeas_pin 4
 #define invVoltMeas_pin 4
 #define PWMPosOut_pin 12
 #define PWMNegOut_pin 32
@@ -29,10 +30,10 @@
 #define PWMNegOutChannel 1
 #define PWMResolution 8
 
-#define measDeadZone 0.2
+#define measDeadZone 0.5
 
 //define variables
-int netVoltMeas, invVoltMeas, highestValue, loopTimer;
+int netFreqVoltMeas, netPhaseVoltMeas, invVoltMeas, highestValue, loopTimer;
 double netFreq;
 bool signal;
 //unsigned long to not overflow the buffer
@@ -41,7 +42,7 @@ unsigned long currentMillis, zeroPointMillis, timerMillis;
 char printBuffer[100];
 
 CircularBuffer<int, 5> voltMeasBuf; //circular buffer of the voltage measurements
-CircularBuffer<unsigned long, 6> zeroPointMillisBuf; //circular buffer of the zeroPoits in ms
+CircularBuffer<unsigned long, 11> zeroPointMillisBuf; //circular buffer of the zeroPoits in ms
 
 //Define PID Variables we'll be connecting to
 double freqSetPoint, Input, freqOutput;
@@ -53,7 +54,8 @@ PID PWMPID(&netFreq, &freqOutput, &freqSetPoint, PWMPIDp, PWMPIDi, PWMPIDd, DIRE
 void setup() {  
   //set pin modes
   pinMode(LED_pin, OUTPUT);
-  pinMode(netVoltMeas_pin, INPUT);
+  pinMode(netFreqVoltMeas_pin, INPUT);
+  pinMode(netPhaseVoltMeas_pin, INPUT);
 
   // configure LED PWM functionalitites
   ledcSetup(PWMPosOutChannel, PWMFreq, PWMResolution);
@@ -77,44 +79,45 @@ void setup() {
 
 int readSensors(){
   //measure and save the voltage measurements
-  netVoltMeas = analogRead(netVoltMeas_pin);
-  invVoltMeas = analogRead(invVoltMeas_pin);
+  netFreqVoltMeas = analogRead(netFreqVoltMeas_pin);
+  netPhaseVoltMeas = analogRead(netPhaseVoltMeas_pin);
 
   #if DEBUG_readings
-    sprintf(printBuffer, "netVoltMeas : %d", netVoltMeas);
+    sprintf(printBuffer, "netFreqVoltMeas : %d", netFreqVoltMeas);
     Serial.println(printBuffer);
   #endif
 
   return 0;
 }
 
-int calculateFrequency(){
+double calculateFrequency(){
   //calculate the frequency of the net based of the timing between the high points in the measured sinusoidal signal from the net
 
-  if (netVoltMeas > highestValue){
-      highestValue = netVoltMeas;
+  if (netFreqVoltMeas > highestValue){
+      highestValue = netFreqVoltMeas;
         #if DEBUG_freqCalc
           sprintf(printBuffer, "highestValue : %d", highestValue);
           Serial.println(printBuffer);
         #endif
     }
 
-  if (netVoltMeas < (0+measDeadZone*highestValue) && signal == 1){
+  if (netFreqVoltMeas < (0+measDeadZone*highestValue) && signal == 1){
     digitalWrite(LED_pin,0);
     zeroPointMillisBuf.push(currentMillis);
     signal = 0;
-  } else if (netVoltMeas > highestValue-(measDeadZone*highestValue)){
+  } else if (netFreqVoltMeas > highestValue-(measDeadZone*highestValue)){
     digitalWrite(LED_pin,1);
     signal = 1;
   }
 
   if (zeroPointMillisBuf.isFull()){
     netFreq = 5000/(zeroPointMillisBuf.last() - zeroPointMillisBuf.first());
+    netFreq = 5000/101;
   }
 
   //only print every 250ms
   if ((currentMillis - timerMillis) > 250){
-    sprintf(printBuffer, "Net freq. : %f Hz", netFreq);
+    sprintf(printBuffer, "Net freq. : %lf Hz", netFreq);
     Serial.println(printBuffer);
     timerMillis = currentMillis;
   }
